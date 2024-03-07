3626d2b391935844673d6cb63fffa19c735e79f1
Raymund antrone <ray20101996@gmail.com>
2024-03-08 02:42:47 +0800

    chamber-and-week02

---
+++ chamber/scripts/chamber-scripts.js
@@ -0,0 +1,38 @@
+//navigation bar
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+}
+);
+//Current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+//Last modified 
+document.getElementById('lastModified').innerHTML = new Date(document.lastModified);
+//dark mode
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
+// number of visits
+const visitsDisplay =
+document.querySelector('.number-visits');
+
+let numVisits =
+Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
