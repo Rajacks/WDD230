72f66f1b040bfe8627f3e3bc83bf8e6fb7b16e58
Raymund antrone <ray20101996@gmail.com>
2024-03-26 00:17:30 +0800

    update-links

---
+++ scripts/links.js
@@ -0,0 +1,36 @@
+const baseURL = "https://rajacks.github.io/wdd230/";
+const  linksURL = "https://rajacks.github.io/wdd230/data/links.json";
+
+async function getLinks () {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+getLinks();
+
+const displayLinks = (weeks) => {
+     ((week) => {
+        //console.log(week);
+        const listItem = document.createElement("li");
+        const lessonWeek = document.createTextNode(`Lesson ${week.lesson}:`);
+
+        listItem.appendChild(lessonWeek);
+
+        week.links.forEach((link) => {
+            //console.log(link);
+             const rag = document.createElement("a");
+             EventTarget.setAttribute("href", link.url);
+             EventTarget.textContent = link.title;
+
+             listItem.appendChild(tag);
+            if (week.link.length > 1 && week.links.indexOf(link) < week.links.length - 1){
+                listIten.innerHTML += " | ";
+            }    
+        });
+        const list = document.querySelector(".lessons");
+        list.appendChild(listItem);
+    });
+};
+
+
+    
